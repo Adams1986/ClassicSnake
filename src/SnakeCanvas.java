a597d7789f3db3313f51e25eda542e87a9175f18
Adams1986 <simonadams86@gmail.com>
2015-10-22 22:48:12 +0200

    Finished tutorial!

--- src/SnakeCanvas.java
+++ src/SnakeCanvas.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -22,33 +25,100 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
     //god-object. Runs methods in background
     private Thread runThread;
     private int score = 0;
+    private String highscore = getHighScore();
 
+    private Image menuImage = null;
+    private boolean isInMenu = true;
+    private boolean isAtEndGame = false;
+    private boolean hasWon = false;
+    private int highScoreInt = -1;
 
     public void paint(Graphics g){
 
-        setPreferredSize(new Dimension(640, 480));
-        addKeyListener(this);
-
         //only resetting snake when exists. Update method calls paint method so important that new List not generated
         //all the time -- snake reset alot
-        if (snake == null) {
-            snake = new LinkedList<Point>();
-
-            generateDefaultSnake();
-            placeFruit();
-        }
-
         if (runThread == null){
 
+            setPreferredSize(new Dimension(640, 480));
+            addKeyListener(this);
             //this thread
             runThread = new Thread(this);
             runThread.start();
         }
 
-        drawFruit(g);
-        drawBoard(g);
-        drawSnake(g);
-        drawScore(g);
+        if (isInMenu){
+            //draw menu
+            drawMenu(g);
+            System.out.println(isAtEndGame+""+hasWon);
+        }
+        else if (isAtEndGame){
+
+            //show end game
+            drawEndGame(g);
+            hasWon = false;
+            isAtEndGame = true;
+        }
+        else {
+
+            if (snake == null) {
+                snake = new LinkedList<Point>();
+
+                generateDefaultSnake();
+                placeFruit();
+            }
+
+            if (highscore.equals("")){
+
+                highscore = getHighScore();
+            }
+            drawFruit(g);
+            drawBoard(g);
+            drawSnake(g);
+            drawScore(g);
+        }
+    }
+
+    private void drawEndGame(Graphics g) {
+
+        BufferedImage endGameImage = new BufferedImage(
+                getPreferredSize().width, getPreferredSize().height, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics endGameGraphics = endGameImage.getGraphics();
+        endGameGraphics.setColor(Color.BLACK);
+
+        if (hasWon) {
+            endGameGraphics.drawString(
+                    "You won. Do you even have a life?", getPreferredSize().width/2, getPreferredSize().height/2);
+        }
+        else{
+
+            if(highScoreInt != -1){
+                endGameGraphics.drawString(
+                        "Congratulations " + highscore.split(":")[0] + " on setting a new high score!", getPreferredSize().width / 2, getPreferredSize().height / 2);
+            }
+            endGameGraphics.drawString(
+                    "You lost. Try again", getPreferredSize().width/2, getPreferredSize().height/2 + 30);
+        }
+        endGameGraphics.drawString("Your score: " + score, getPreferredSize().width/2, getPreferredSize().height/2 + 60);
+        endGameGraphics.drawString(
+                "Press \"SPACE\" to start again", getPreferredSize().width/2, getPreferredSize().height/2 + 90);
+
+        g.drawImage(endGameImage, 0, 0, this);
+    }
+
+    private void drawMenu(Graphics g){
+
+        if (menuImage == null){
+
+            try {
+                URL imagePath = SnakeCanvas.class.getResource("/teamdepardieu.png");
+                menuImage = Toolkit.getDefaultToolkit().getImage(imagePath);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        g.drawImage(menuImage, 0, 0, 640, 480, this);
     }
 
     //contains the double buffering
@@ -91,11 +161,12 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
 
         while (true){
 
-            //game continues loop to keep running logic
-            move();
-
             //calls update and paint method
             repaint();
+            //game continues loop to keep running logic
+            if(!isInMenu && !isAtEndGame)
+                move();
+
 
             try {
                 //access to current thread from run-method
@@ -112,6 +183,7 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
 
     private void move(){
 
+        if (direction == Direction.NO_DIRECTION) return;
         //grab first item of list
         Point head = snake.peekFirst();
         Point newPoint = head;
@@ -142,6 +214,7 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
 
         //removing last part of snake before adding below.
         //TODO: Maybe not remove for client/server project
+        if (direction!=Direction.NO_DIRECTION)
         snake.remove(snake.peekLast());
 
         if (newPoint.equals(fruit)){
@@ -186,20 +259,34 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
         else if (newPoint.x < 0 || newPoint.x > BOARD_WIDTH - 1){
 
             //OOB
-            generateDefaultSnake();
+            checkScore();
+            hasWon = false;
+            isAtEndGame = true;
             return;
 
         }
         else if (newPoint.y < 0 || newPoint.y > BOARD_HEIGHT - 1){
 
             //OOB
-            generateDefaultSnake();
+            checkScore();
+            hasWon = false;
+            isAtEndGame = true;
             return;
         }
         else if (snake.contains(newPoint)){
 
             //gonna have a bad time. cannibalism?!!!!
-            generateDefaultSnake();
+            checkScore();
+            hasWon = false;
+            isAtEndGame = true;
+            return;
+        }
+        else if (snake.size() == (BOARD_HEIGHT * BOARD_WIDTH)){
+
+            checkScore();
+            hasWon = true;
+            isAtEndGame = true;
+            //filled out all fields
             return;
         }
 
@@ -247,6 +334,7 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
 
         g.setColor(Color.LIGHT_GRAY);
         g.drawString("Score: " + score, 0, FIELD_HEIGHT * BOARD_HEIGHT + 20);
+        g.drawString("Highscore: " + highscore, 0, FIELD_HEIGHT * BOARD_HEIGHT + 35);
         g.setColor(Color.BLACK);
     }
 
@@ -268,6 +356,80 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
         fruit = randomPoint;
     }
 
+    private String getHighScore(){
+
+        FileReader readFile = null;
+        BufferedReader reader = null;
+
+        //format: Brandon:100 json?
+        try {
+            readFile = new FileReader("highscore.dat");
+            reader = new BufferedReader(readFile);
+
+            return reader.readLine();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "everyone:0";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "everyone:0";
+        }
+        finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void checkScore(){
+
+        if (highscore.equals("")) return;
+
+        highScoreInt = Integer.parseInt(highscore.split(":")[1]);
+
+        if (score > highScoreInt){
+
+            //better solution!
+            String name = JOptionPane.showInputDialog(this,"Congrats, new high score. Enter name: ");
+            highscore = name + ":" + score;
+
+
+            File scoreFile = new File("highscore.dat");
+            System.out.println(scoreFile.getAbsolutePath());
+
+            if (!scoreFile.exists()){
+                try {
+                    scoreFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            FileWriter writeFile = null;
+            BufferedWriter writer = null;
+
+            try {
+                writeFile = new FileWriter(scoreFile);
+                writer = new BufferedWriter(writeFile);
+                writer.write(highscore);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if(writer != null)
+                        writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
     }
@@ -279,22 +441,42 @@ public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
 
             case KeyEvent.VK_UP:
                 if (direction != Direction.DOWN)
-                direction = Direction.UP;
+                    direction = Direction.UP;
                 break;
 
             case KeyEvent.VK_DOWN:
                 if (direction != Direction.UP)
-                direction = Direction.DOWN;
+                    direction = Direction.DOWN;
                 break;
 
             case KeyEvent.VK_LEFT:
                 if (direction != Direction.RIGHT)
-                direction = Direction.LEFT;
+                    direction = Direction.LEFT;
                 break;
 
             case KeyEvent.VK_RIGHT:
                 if (direction != Direction.LEFT)
-                direction = Direction.RIGHT;
+                    direction = Direction.RIGHT;
+                break;
+
+            case KeyEvent.VK_ENTER:
+                if(isInMenu){
+                    isInMenu = false;
+                    repaint();
+                }
+                break;
+
+            case KeyEvent.VK_ESCAPE:
+                isInMenu = true;
+                break;
+
+            case KeyEvent.VK_SPACE:
+                if (isAtEndGame) {
+                    isAtEndGame = false;
+                    hasWon = false;
+                    generateDefaultSnake();
+                    repaint();
+                }
                 break;
         }
     }
